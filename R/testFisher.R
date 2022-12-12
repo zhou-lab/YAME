02cecb1af2cf96fff5136e48400c23d734db8ffd
Wanding Zhou <zhouw3@email.chop.edu>
2022-12-11 23:33:28 -0500

    minor, message omitted

--- R/testFisher.R
+++ R/testFisher.R
@@ -2,12 +2,13 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 suppressMessages(library(dplyr))
-options(scipen=99)
-options(digits=3)
 df = read.table(args[1], header=F)
 colnames(df)[1:4] = c('nU','nD','nQ','nDQ')
 suppressMessages(library(sesame))
 df2 = with(df, sesame:::testEnrichmentFisherN(nD, nQ, nDQ, nU))
 df2 = cbind(df[,colnames(df)[!(colnames(df) %in% c("nD","nQ","nDQ"))],drop=F],df2)
+options(scipen=10,digits=5)
 write.table(format(df2), file="", quote=FALSE, row.names=FALSE, sep="\t")
+## suppressMessages(library(readr))
+## cat(readr::format_tsv(df2))
 
