c0c1e7b661b22d2a60ad0b11785a7a10b98475f1
Wanding Zhou <zhouw3@email.chop.edu>
2022-12-11 22:08:42 -0500

    minor, message omitted

--- R/testFisher.R
+++ R/testFisher.R
@@ -5,14 +5,9 @@ suppressMessages(library(dplyr))
 options(scipen=99)
 options(digits=3)
 df = read.table(args[1], header=F)
-colnames(df)[1:4] = c('nu','nf','nq','nfq')
-df = df %>% dplyr::mutate(
-    nfmq = nf - nfq,
-    nqmf = nq - nfq,
-    numfq = nu - nf - nq + nfq,
-    ## https://seqqc.wordpress.com/2019/07/25/how-to-use-phyper-in-r/
-    p_val_enr = sprintf("%1.3g", phyper(nfq-1, nf, nu-nf, nq, lower.tail=FALSE)),
-    p_val_dep = sprintf("%1.3g", phyper(nfq, nf, nu-nf, nq, lower.tail=TRUE)),
-    odds_ratio=(nfq/nfmq)/(nqmf/numfq)) %>% dplyr::arrange(as.numeric(p_val_enr))
-write.table(format(df), file="", quote=FALSE, row.names=FALSE, sep="\t")
+colnames(df)[1:4] = c('nU','nD','nQ','nDQ')
+suppressMessages(library(sesame))
+df2 = with(df, sesame:::testEnrichmentFisherN(nD, nQ, nDQ, nU))
+df2 = cbind(df[,colnames(df)[!(colnames(df) %in% c("nD","nQ","nDQ"))],drop=F],df2)
+write.table(format(df2), file="", quote=FALSE, row.names=FALSE, sep="\t")
 
