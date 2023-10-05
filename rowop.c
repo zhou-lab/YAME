60650f7b187c343122f3811d054cf5bf190c4038
Wanding Zhou <zhouw3@email.chop.edu>
2023-10-04 23:16:04 -0400

    added musum to rowop

--- rowop.c
+++ rowop.c
@@ -11,6 +11,7 @@ static int usage() {
   fprintf(stderr, "    -v        verbose\n");
   fprintf(stderr, "    -o        operations: {binasum}\n");
   fprintf(stderr, "              binasum: sum binary data to M and U (format 3).\n");
+  fprintf(stderr, "              musum: sum M and U separately (format 3).\n");
   fprintf(stderr, "              mean: mean beta and counts of data points.\n");
   fprintf(stderr, "    -c        minimum sequencing depth for rowops (default 1).\n");
   fprintf(stderr, "    -h        This help\n");
@@ -75,6 +76,55 @@ static cdata_t rowop_binasum(cfile_t cf, unsigned mincov) {
     case '0': binasumFmt0(&cout, &c2); break;
     case '1': binasumFmt0(&cout, &c2); break;
     case '3': binasumFmt3(&cout, &c2, mincov); break;
+    default: {
+      fprintf(stderr, "[%s:%d] File format: %c unsupported.\n", __func__, __LINE__, c.fmt);
+      fflush(stderr);
+      exit(1);
+    }}
+
+    free(c.s); free(c2.s);
+  }
+  return cout;
+}
+
+static void musumFmt3(cdata_t *cout, cdata_t *c) {
+  for (uint64_t i=0; i<c->n; ++i) {
+    uint64_t mu0 = f3_get_mu(c, i);
+    if (!mu0) continue; // 0-0 is skipped
+    uint64_t mu = f3_get_mu(cout, i);
+    f3_set_mu(cout, i, ((mu>>32)+(mu0>>32)), ((mu<<32>>32)+(mu0<<32>>32)));
+  }
+}
+
+static cdata_t rowop_musum(cfile_t cf) {
+  cdata_t c = read_cdata1(&cf);
+  cdata_t cout = {0};
+  if (c.n == 0) return cout;    // nothing in cfile
+  char fmt = c.fmt;
+  cout.n = cdata_n(&c);
+  cout.compressed = 0;
+  cout.fmt = '3';
+  cout.unit = 8;                // max-size result
+  cout.s = calloc(cout.n, sizeof(uint64_t));
+  
+  for (uint64_t k=0; ; ++k) {
+    if (k) c = read_cdata1(&cf); // skip 1st cdata
+    if (c.n == 0) break;
+    if (fmt != c.fmt) {
+      fprintf(stderr, "[%s:%d] File formats are inconsistent: %c vs %c.\n", __func__, __LINE__, fmt, c.fmt);
+      fflush(stderr);
+      exit(1);
+    }
+    cdata_t c2 = {0};
+    decompress(&c, &c2);
+    if (c2.n != cout.n) {
+      fprintf(stderr, "[%s:%d] Data dimensions are inconsistent: %"PRIu64" vs %"PRIu64"\n", __func__, __LINE__, cout.n, c2.n);
+      fflush(stderr);
+      exit(1);
+    }
+    
+    switch (fmt) {
+    case '3': musumFmt3(&cout, &c2); break;
     default: {
       fprintf(stderr, "[%s:%d] File format: %c unsupported.\n", __func__, __LINE__, c.fmt);
       fflush(stderr);
@@ -173,6 +223,10 @@ int main_rowop(int argc, char *argv[]) {
     free(cout.s);
   } else if (strcmp(op, "mean") == 0) {
     rowop_mean(cf, fname_out, mincov);
+  } else if (strcmp(op, "musum") == 0) {
+    cout = rowop_musum(cf);
+    cdata_write(fname_out, &cout, "wb", verbose);
+    free(cout.s);
   } else {
     fprintf(stderr, "[%s:%d] Unsupported operation: %s\n", __func__, __LINE__, op);
     fflush(stderr);
