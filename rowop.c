0f78a60fb94f01a87f52e5c61b4f69df7dae370f
Wanding Zhou <zhouwanding@gmail.com>
2023-12-09 11:29:10 -0500

    minor, message omitted

--- rowop.c
+++ rowop.c
@@ -10,10 +10,11 @@ static int usage() {
   fprintf(stderr, "Options:\n");
   fprintf(stderr, "    -v        verbose\n");
   fprintf(stderr, "    -o        operations: {binasum}\n");
-  fprintf(stderr, "              binasum: sum binary data to M and U (format 3).\n");
-  fprintf(stderr, "              musum: sum M and U separately (format 3).\n");
-  fprintf(stderr, "              mean: mean beta and counts of data points.\n");
-  fprintf(stderr, "              binstring: binarize data to a row-wise string.\n");
+  fprintf(stderr, "                  binasum: sum binary data to M and U (format 3).\n");
+  fprintf(stderr, "                  musum: sum M and U separately (format 3).\n");
+  fprintf(stderr, "                  mean: mean beta and counts of data points.\n");
+  fprintf(stderr, "                  std: standard deviation.\n");
+  fprintf(stderr, "                  binstring: binarize data to a row-wise string.\n");
   fprintf(stderr, "    -c        minimum sequencing depth for rowops (default 1).\n");
   fprintf(stderr, "    -h        This help\n");
   fprintf(stderr, "\n");
@@ -192,6 +193,69 @@ static void rowop_mean(cfile_t cf, char *fname_out, unsigned mincov) {
   if (fname_out) fclose(out);
 }
 
+static void sumsqFmt3(uint32_t *cnts, double *sum, double *sum_sq, cdata_t *c, unsigned mincov) {
+  for (uint64_t i=0; i<c->n; ++i) {
+    uint64_t mu0 = f3_get_mu(c, i);
+    if (!mu0) continue; // 0-0 is skipped
+    if (((mu0>>32) + (mu0<<32>>32)) < mincov) continue;
+    uint64_t M = mu0>>32;
+    uint64_t U = (mu0<<32>>32);
+    if (M+U >= mincov) {
+      double x = (double) M / (M+U);
+      sum[i] += x;
+      sum_sq[i] += x * x;
+      cnts[i]++;
+    }
+  }
+}
+
+
+// the following doesn't work for large numbers but should be ok for meth levels
+// see https://www.strchr.com/standard_deviation_in_one_pass
+static void rowop_std(cfile_t cf, char *fname) {
+
+  cdata_t c = read_cdata1(&cf);
+  if (c.n == 0) return; // nothing in cfile, output nothing
+  uint64_t n = cdata_n(&c);
+  uint32_t *cnts = calloc(n, sizeof(uint32_t));
+  double sum = calloc(n, sizeof(double));
+  double sum_sq = calloc(n, sizeof(double));
+  
+  for (uint64_t k = 0; ; ++k) {
+    if (k) c = read_cdata1(&cf); // skip 1st cdata
+    if (c.n == 0) break;
+    cdata_t c2 = {0};
+    decompress(&c, &c2);
+
+    switch (c.fmt) {
+    case '3': sumsqFmt3(cnts, sum, sum_sq, &c2, mincov); break;
+    default: {
+      fprintf(stderr, "[%s:%d] File format: %c unsupported.\n", __func__, __LINE__, c.fmt);
+      fflush(stderr);
+      exit(1);
+    }}
+
+    free(c.s); free(c2.s);
+  }
+
+  FILE *out;
+  if (fname_out) {
+    out = fopen(fname_out, "w");
+  } else {
+    out = stdout;
+  }
+  for (uint64_t i=0; i<n; ++i) {
+    if (!cnts[i]) {
+      fputs("NA\t0\n", out);
+    } else {
+      double mean = sum[i] / cnts[i];
+      fprintf(out, "%1.3f\t%d\n", sqrt((sum_sq[i] / cnts[i]) - mean * mean), cnts[i]);
+    }
+  }
+  free(cnts); free(fracs);
+  if (fname_out) fclose(out);
+}
+
 static void rowop_binstring(cfile_t cf, char *fname_out) {
   cdata_t c = read_cdata1(&cf);
   if (c.n == 0) return;    // nothing in cfile
@@ -276,6 +340,8 @@ int main_rowop(int argc, char *argv[]) {
     free(cout.s);
   } else if (strcmp(op, "mean") == 0) {
     rowop_mean(cf, fname_out, mincov);
+  } else if (strcmp(op, "std") == 0) {
+    rowop_std(cf, fname_out, mincov);
   } else if (strcmp(op, "musum") == 0) {
     cout = rowop_musum(cf);
     cdata_write(fname_out, &cout, "wb", verbose);
